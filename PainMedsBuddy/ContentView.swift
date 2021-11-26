6566824905cd8eca2309fd0d323ddb10cff56ef0
JulesMoorhouse <361024+JulesMoorhouse@users.noreply.github.com>
2021-11-26 15:10:26 +0000

    Initial files.

---
+++ PainMedsBuddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PainMedsBuddy
+//
+//  Created by Jules Moorhouse.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PainMedsBuddy/PainMedsBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PainMedsBuddyApp.swift
+//  PainMedsBuddy
+//
+//  Created by Jules Moorhouse.
+//
+
+import SwiftUI
+
+@main
+struct PainMedsBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
